4a2c475bccef14002d1313c19f87ac18c21ab89c
Slava Poliakov <78slavapolyakov@gmail.com>
2024-03-28 19:15:08 +0200

    Added files for task 3

---
+++ task_3/query.sql
@@ -0,0 +1,15 @@
+SELECT 
+	order_id, o.date AS date,
+    cust.name AS customer, cust.contact AS contact_person, cust.city AS city,
+    p.name AS product, cat.name AS category, p.price AS price, quantity,
+    e.last_name AS sales_manager, sh.name AS shipper, sh.phone AS shipper_phone, 
+    sup.name AS supplyer, sup.contact AS supplyer_person, sup.city AS supplyer_city, sup.phone AS supplyer_phone
+FROM order_details AS od
+INNER JOIN orders AS o ON o.id = od.order_id
+INNER JOIN customers AS cust ON cust.id = o.customer_id
+INNER JOIN products AS p ON od.product_id = p.id
+INNER JOIN categories AS cat ON p.category_id = cat.id
+INNER JOIN employees AS e ON o.employee_id = e.employee_id
+INNER JOIN shippers AS sh ON o.shipper_id = sh.id
+INNER JOIN suppliers AS sup ON p.supplier_id = sup.id
+ORDER BY order_id, date;
